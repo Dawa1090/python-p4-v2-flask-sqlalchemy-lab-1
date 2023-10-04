2593f2b6b23cef84a3761ee859a4feb9edadf821
Dawa Sherpa <dawasherpa1090@gmail.com>
2023-10-04 12:32:41 -0400

    lab 1

--- server/app.py
+++ server/app.py
@@ -6,6 +6,7 @@ from flask_migrate import Migrate
 
 from models import db, Earthquake
 
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -21,6 +22,25 @@ def index():
     return make_response(body, 200)
 
 # Add views here
+@app.route('/earthquakes/<int:id>')
+def show_earthquake(id):
+    earthquake = Earthquake.query.filter_by(id=id).first()
+    if earthquake:
+        return make_response(earthquake.to_dict())
+    else:
+        return make_response({"message": f"Earthquake {id} not found."}, 404)
+    
+
+
+@app.route('/earthquakes/magnitude/<float:magnitude>')
+def get_quakes_by_magnitude(magnitude):
+    earthquakes = []
+    for earthquake in Earthquake.query.filter(Earthquake.magnitude >= magnitude).all():
+        earthquakes.append(earthquake.to_dict())
+
+    return make_response({"count": len(earthquakes), "quakes": earthquakes}, 200)
+
+
 
 
 if __name__ == '__main__':
